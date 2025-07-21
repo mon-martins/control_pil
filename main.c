edffa1af4311695b8298e09fc0249d2c213b1523
Ramon Martins <ramon.martins@engenharia.ufjf.br>
2025-07-21 14:37:49 -0300

    Ao otro maxado

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 #include "board.h"
 #include "scicomm.h"
 
-#define B0 0.001033
-#define B1 0.000767
-#define B2 -0.000266
-#define A1 -1.521886
-#define A2 0.521886
+#define B0 0.000386f
+#define B1 0.000117f
+#define B2 -0.000269f
+#define A1 -1.521886f
+#define A2 0.521886f
 
 #define REF 20
 #define UL 1
@@ -18,7 +18,7 @@
 
 float x[3]={0};
 float y[3]={0};
-float vo;
+float erro_calc;
 
 //
 // Função Principal
@@ -39,9 +39,9 @@ void main(void)
     {
         if (SCI_getRxFIFOStatus(SCI0_BASE)>3)
         {
-            protocolReceiveData(SCI0_BASE,&vo,sizeof(float));
+            protocolReceiveData(SCI0_BASE,&erro_calc,sizeof(float));
 
-            x[2] = x[1]; x[1] = x[0]; x[0] = REF - vo;
+            x[2] = x[1]; x[1] = x[0]; x[0] = erro_calc;
             y[2] = y[1]; y[1] = y[0];
             y[0] = B0*x[0] + B1*x[1] + B2*x[2] - A1*y[1] - A2*y[2];
 
--- python/projeto_controlador.py
+++ python/projeto_controlador.py
@@ -5,19 +5,29 @@ from matplotlib import pyplot as plt
 
 #Parâmetros
 fsw = 20000
-Vin = 48
+Vin = 24
 L = 400e-6
 C = 47e-6
 R = 5
+
 vcp = 1
+
 Tp = 1/(2*np.pi*fsw*0.1)
 
-PM = 60  #graus
-FC = 600 #Hz
+PM = 30  #graus
+FC = 5000 #Hz
 
 #Cálculo do controlador no domínio da frequência
 
 Gp = control.tf([Vin],[L*C,L/R,1])
+
+# A_1 = [ [0 , -0.75/(400e-6)] , [0.75/(47e-6) , -1/(5*47e-6)] ]
+# B_1 = [ [32/(400e-6)] , [-((32/5)/(0.75))/(47e-6)] ]
+# C_1 = [0 , 1]
+# D_1 = [0]
+
+# Gp = control.ss(A_1,B_1,C_1,D_1)
+
 Gpwm = control.tf([1],[vcp])
 Gs = control.tf([1],[1])
 
@@ -39,6 +49,14 @@ control.bode(Gol(k,T),omega,Hz=True,dB=True,deg=True)
 gm, pm, wcg, wcp = control.margin(Gol(k,T))
 print(f'Margem de fase: {pm:.2f}º Frequência de cruzamento: {wcp/(2*np.pi):.2f}')
 print(f'Parâmetros: k = {k} | T = {T}')
+
+print(f'Parâmetros: kp = {k} | ki = {k/T}')
+plt.show()
+
+G_MF = control.feedback(control.series(Gc(k,T),Gpwm,Gp) , Gs)
+t_out , y_out = control.step_response(G_MF)
+
+plt.plot(t_out,y_out)
 plt.show()
 
 #Discretização do controlador
@@ -54,4 +72,5 @@ for i in range(order):
 for i in range(1,order):
     output += f"#define A{i} {controller.den[0][0][i]/controller.den[0][0][0]:.6f}\r\n"
 print(output)
- 
+
+
